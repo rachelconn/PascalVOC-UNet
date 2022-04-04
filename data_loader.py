e5aaf6601b84252d41df975120d175826149b21e
Ryan Conn <rconn478@gmail.com>
2022-04-04 15:30:41 -0500

    Add support for outcrop dataset

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,6 @@
 import os
+import random
+import numpy as np
 import tensorflow as tf
 
 TRAIN_VAL_DATASET_PATH = os.path.abspath(os.path.join('datasets', 'VOC2012'))
@@ -9,7 +11,7 @@ def _get_x_y(label_path):
     image_path = tf.strings.regex_replace(image_path, '.png', '.jpg')
     return image_path, label_path
 
-def load_dataset(path=TRAIN_VAL_DATASET_PATH):
+def load_pascal_dataset(path=TRAIN_VAL_DATASET_PATH):
     # Load datset
     file_ds = tf.data.Dataset.list_files(os.path.join(LABEL_DIRECTORY, '*'), shuffle=False)
     dataset = file_ds.map(_get_x_y)
@@ -26,3 +28,49 @@ def load_dataset(path=TRAIN_VAL_DATASET_PATH):
     test_ds = dataset.skip(train_size).skip(validation_size)
 
     return train_ds, validation_ds, test_ds
+
+def parse_outcrop_label_file(filename):
+    with open(filename) as f:
+        resolution, _, mask = f.readlines()
+        width, height = (int(x) for x in resolution.split(','))
+        mask = np.array(mask.split(','), dtype=np.int16).reshape(width, height, 2)
+        return mask
+
+def generate_outcrop_dataset(path):
+    x_dir = os.path.join(path, b'x')
+    y_dir = os.path.join(path, b'y')
+    x_basenames = os.listdir(x_dir)
+    random.shuffle(x_basenames)
+    for x_basename in x_basenames:
+        x = tf.keras.utils.load_img(os.path.join(x_dir, x_basename))
+        x = np.array(x) / 255
+
+        y_filename = os.path.join(y_dir, os.path.splitext(x_basename)[0] + b'.csv')
+        y = parse_outcrop_label_file(y_filename)
+
+        yield x, y
+
+def load_outcrop_dataset(path, train_split=0.8, val_split=0.1, test_split=0.1):
+    # Calculate length to create splits
+    dataset_size = len(os.listdir(os.path.join(path, 'x')))
+    train_size = int(train_split * dataset_size)
+    val_size = int(val_split * dataset_size)
+    test_size = int(test_split * dataset_size)
+
+    # Create dataset
+    dataset = tf.data.Dataset.from_generator(
+        generate_outcrop_dataset,
+        args=(path,),
+        output_signature=(
+            tf.TensorSpec(shape=(None, None, 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(None, None, 2), dtype=tf.int16),
+        ),
+    )
+
+    # Split into train, val, test sets
+    train_dataset = dataset.take(train_size)
+    val_dataset = dataset.skip(train_size)
+    test_dataset = val_dataset.skip(val_size)
+    val_dataset = test_dataset.take(val_size)
+
+    return train_dataset, val_dataset, test_dataset
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from data_loader import load_dataset
+import os
+from data_loader import load_pascal_dataset, load_outcrop_dataset
 from model import Model
 from config import default_model_params, default_training_params
 
+OUTCROP_DATASET_FOLDER = os.path.join('E:', 'outcrop')
+
 if __name__ == '__main__':
-    train_ds, validation_ds, test_ds = load_dataset()
+    train_ds, validation_ds, test_ds = load_pascal_dataset()
     model = Model(default_model_params, default_training_params)
 
     model.train(train_ds, validation_ds)
 
     model.test(test_ds)
+
+    # TODO: swap to this code instead when we feel comfortable using the outcrop dataset
+    # train_ds, validation_ds, test_ds = load_outcrop_dataset(OUTCROP_DATASET_FOLDER)
+    # for x, y in train_ds:
+    #     print(x.shape, y.shape)
